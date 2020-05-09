95d7c2f09ab6c2e0b7739954f5b84d23173890e4
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-09 18:24:15 -0500

    def create orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,15 @@ class OrdersController < ApplicationController
         @users = User.all
         @items = Item.all
     end
+
+    def create
+        @order = Order.new(order_params)
+        if @order.save
+            redirect_to order_path(@order)
+        else
+            @users = User.all
+            @items = Item.all
+            render :new
+        end
+    end
 end
