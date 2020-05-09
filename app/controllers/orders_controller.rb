b409917ed061d4cfe57ea2620e99ee82636a10d2
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-09 18:55:54 -0500

    created order_items db

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,28 @@
 class OrdersController < ApplicationController
     
     def new
-        @order = Order.new
+      @order = Order.new
+      @users = User.all
+      @items = Item.all
+    end
+  
+    def create
+      @order = Order.new(order_params)
+      if @order.save
+        redirect_to order_path(@order)
+      else
         @users = User.all
         @items = Item.all
+        render :new
+      end
     end
-
-    def create
-        @order = Order.new(order_params)
-        if @order.save
-            redirect_to order_path(@order)
-        else
-            @users = User.all
-            @items = Item.all
-            render :new
-        end
-    end
-
+  
+    # get '/orders/:id'
     def show
-        @order = Order.find(params[:id])
+      @order = Order.find(params[:id])
     end
-
-    private
-
+  
     def order_params
-        params.require(:order).permit(:user_id, item_ids: [])
+      params.require(:order).permit(:user_id, :item_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_225440) do
+ActiveRecord::Schema.define(version: 2020_05_09_235341) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_05_09_225440) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -51,5 +60,7 @@ ActiveRecord::Schema.define(version: 2020_05_09_225440) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
