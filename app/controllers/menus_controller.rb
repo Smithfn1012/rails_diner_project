411b16a164bb622e3766776b7083fdf211152b0f
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-09 17:26:11 -0500

    added some crud to menus_controller.rb

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,33 @@
+class MenusController < ApplicationController
+
+    def new
+        @menu = Menu.new
+        make_blank_items
+    end
+
+    def create
+        @menu = Menu.new(menu_params)
+        make_blank_items
+        if @menu.save
+            redirect_to menu_path(@menu)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @menu = Menu.find(params[:id])
+    end
+
+    private
+
+    def menu_params
+        params.require(:menu).permit(:name, items_attributes: [:price, :name, :description])
+    end
+
+    def make_blank_items
+        @new_items = @menu.tap{|m| 3.times { m.items.build} }.items
+    end
+end
+
+end
