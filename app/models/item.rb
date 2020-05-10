1fdefad38a08878b076ff3716b3f69dfd0428ee4
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-09 19:01:55 -0500

    updated Item model relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
-    belongs_to :menu
+  belongs_to :menu
+  has_many :order_items
+  has_many :orders, through: :order_items
 
-    validates :name, :price, :description, presence: true
+  validates :name, :price, :description, presence: true
 end
