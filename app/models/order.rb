6c3c28f5d9f4f445a7e9a02393127669291499a9
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-09 19:04:32 -0500

    update Order model relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+  has_many :order_items
+  has_many :items, through: :order_items
+
+  validate :order_must_have_at_least_one_item
+
+  def order_must_have_at_least_one_item
+    if item_ids.empty?
+      errors.add(:items, "must exist -- choose at least one!")
+    end
+  end
 end
